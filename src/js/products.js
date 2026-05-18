4a598e4939b31ac6ca9cc58caa5c88f265c266c1
Jared Huayta <ejhuayta@gmail.com>
2026-05-17 23:35:53 -0300

    Week02 - Individual Activity Dynamic Product List

--- src/js/products.js
+++ src/js/products.js
@@ -4,6 +4,5 @@ import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 const productID = getParam("product");
-
 const product = new ProductDetails(productID, dataSource);
 product.init();
