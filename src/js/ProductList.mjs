602e05fb987a9abdad5d599430ba1064dc161b88
Jared Huayta <ejhuayta@gmail.com>
2026-05-20 06:37:26 -0300

    change to temp

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,17 +1,5 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
-/*function productCardTemplate(product) {
-  return `<li class="product-card">
-      <a href="product_pages/?product=${product.Id}">
-        <img src="${product.Image}" alt="${product.Name}">
-        <h2>${product.Brand.Name}</h2>
-        <h3>${product.Name}</h3>
-        <p class="product-card__price">$${product.FinalPrice}</p>
-      </a>
-    </li>
-    `;
-}*/
-
 function productCardTemplate(product) {
   const originalPrice = product.SuggestedRetailPrice;
   const finalPrice = product.FinalPrice;
